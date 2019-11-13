3e79c36263f551e2e2d00351fba0aba031b417d7
Takahiro Morishita <mtaka@astro.ucla.edu>
2019-11-13 15:41:24 -0500

    Update gsf.py

--- gsf/gsf.py
+++ gsf/gsf.py
@@ -199,9 +199,9 @@ def main(parfile, fplt, mcmcplot=True):
 
     if fplt <= 3:
         from .plot_sfh import plot_sfh
-        from .plot_sed import plot_sed_Z
+        from .plot_sed import plot_sed
         plot_sfh(ID0, PA0, Zall, age, f_comp=ftaucomp, fil_path=DIR_FILT, inputs=inputs, dust_model=dust_model, DIR_TMP=DIR_TMP)
-        plot_sed_Z(ID0, PA0, Z=Zall, age=age, tau0=tau0, fil_path=DIR_FILT, SNlim=1.0, figpdf=False, save_sed=True, inputs=inputs, nmc2=300, dust_model=dust_model, DIR_TMP=DIR_TMP)
+        plot_sed(ID0, PA0, Z=Zall, age=age, tau0=tau0, fil_path=DIR_FILT, SNlim=1.0, figpdf=False, save_sed=True, inputs=inputs, nmc2=300, dust_model=dust_model, DIR_TMP=DIR_TMP)
 
     if fplt == 4:
         from .plot_sfh import get_evolv
