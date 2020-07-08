0ffcf119a0efeb3c3a0ddfd36adc952994f6a9ec
Takahiro Morishita <tmorishita@stsci.edu>
2020-07-07 20:09:49 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,8 @@ author = 'Takahiro Morishita'
 # The full version, including alpha/beta/rc tags
 release = '1.2'
 
+master_doc = 'index'
+
 
 # -- General configuration ---------------------------------------------------
 
