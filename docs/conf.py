6699764fd572bf1147eb3ce8d3d23a7d3d48fb88
Takahiro Morishita <tmorishita@stsci.edu>
2021-01-23 14:54:42 -0500

    readdocs

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,31 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
+import sphinx_rtd_theme
+
+extensions = [
+    "sphinx_rtd_theme",
+]
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_options = {
+    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
 # -- Project information -----------------------------------------------------
 
 project = 'gsf'
