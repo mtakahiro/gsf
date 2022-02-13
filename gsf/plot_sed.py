4d7191accddff563cf99f6fa8441ee75a5f4073f
Takahiro Morishita <takahiro@ipac.caltech.edu>
2022-02-12 19:49:22 -0800

    Update plot_sed.py

--- gsf/plot_sed.py
+++ gsf/plot_sed.py
@@ -854,7 +854,11 @@ def plot_sed(MB, flim=0.01, fil_path='./', scale=1e-19, f_chind=True, figpdf=Fal
 
         if f_plot_resid:
             conbb_hs = (fybb/eybb>SNlim)
-            axes['B'].scatter(lbb[iix][con_sed], ((fybb*c/np.square(xbb)/d - fbb)/(eybb*c/np.square(xbb)/d))[iix][con_sed], lw=0.5, color='none', edgecolor='r', zorder=3, alpha=1.0, marker='.', s=50)
+            axes['B'].scatter(lbb[iix][conbb_hs], ((fybb*c/np.square(xbb)/d - fbb)/(eybb*c/np.square(xbb)/d))[iix][conbb_hs], lw=0.5, color='none', edgecolor='r', zorder=3, alpha=1.0, marker='.', s=50)
+            conbb_hs = (fybb/eybb<=SNlim) & (eybb>0)
+            axes['B'].errorbar(lbb[iix][conbb_hs], ((eybb*c/np.square(xbb)/d - fbb)/(eybb*c/np.square(xbb)/d))[iix][conbb_hs], yerr=leng,\
+                uplims=((fybb*c/np.square(xbb)/d - fbb)/(eybb*c/np.square(xbb)/d))[iix][conbb_hs] * sigma, linestyle='',\
+                color=col_dat, lw=0.5, marker='', ms=4, label='', zorder=4, capsize=1.5)
             axes['B'].set_xscale(ax1.get_xscale())
             axes['B'].set_xlim(ax1.get_xlim())
             axes['B'].set_xticks(ax1.get_xticks())
@@ -2071,6 +2075,10 @@ def plot_sed_tau(MB, flim=0.01, fil_path='./', scale=1e-19, f_chind=True, figpdf
         if f_plot_resid:
             conbb_hs = (fybb/eybb>SNlim)
             axes['B'].scatter(lbb[iix][con_sed], ((fybb*c/np.square(xbb)/d - fbb)/(eybb*c/np.square(xbb)/d))[iix][con_sed], lw=0.5, color='none', edgecolor='r', zorder=3, alpha=1.0, marker='.', s=50)
+            conbb_hs = (fybb/eybb<=SNlim) & (eybb>0)
+            axes['B'].errorbar(lbb[iix][conbb_hs], ((eybb*c/np.square(xbb)/d - fbb)/(eybb*c/np.square(xbb)/d))[iix][conbb_hs], yerr=leng,\
+                uplims=((fybb*c/np.square(xbb)/d - fbb)/(eybb*c/np.square(xbb)/d))[iix][conbb_hs] * sigma, linestyle='',\
+                color=col_dat, lw=0.5, marker='', ms=4, label='', zorder=4, capsize=1.5)
             axes['B'].set_xscale(ax1.get_xscale())
             axes['B'].set_xlim(ax1.get_xlim())
             axes['B'].set_xticks(ax1.get_xticks())
