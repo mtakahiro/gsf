475be9fc35eebe2f5f3f760a9287084da2da95b9
Takahiro Morishita <takahiro@ipac.caltech.edu>
2022-09-28 13:30:18 -0700

    pytest complete

--- gsf/tests/test_fitting.py
+++ gsf/tests/test_fitting.py
@@ -9,10 +9,9 @@ def test_fitting():
     '''
     parfile = 'gds_43114.input'
     fplt = 0
-    id = 0
     z = 1.9
     delwave = 5.0
 
     os.chdir('./example/')
 
-    gsf.run_gsf_all(parfile, fplt, nthin=1, delwave=delwave, zman=z) #, idman=id
+    gsf.run_gsf_all(parfile, fplt, nthin=1, delwave=delwave, zman=z)
